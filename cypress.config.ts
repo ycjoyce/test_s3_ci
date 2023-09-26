5d889eba1e7f642483b7718e1e9a5a91c9894646
Joyce <joyce@ecrowdmedia.com>
2023-09-26 09:57:01 +0800

    測試 artifacts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "cypress";
+import codeCoverageTask from "@cypress/code-coverage/task";
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      codeCoverageTask(on, config);
+      return config;
+    },
   },
   component: {
     devServer: {
